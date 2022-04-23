3707122fd416798ee8752d3a49883463a2fa03c1
Rylan <ornelasrylan@rocketmail.com>
2022-04-22 19:58:08 -0700

    Added a draw function and color picker

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 const grid = document.querySelector(".grid");
 const input = document.getElementById("input");
 const reset = document.getElementById("reset");
+const gridSize = document.querySelector(".gridsize");
+let color = document.getElementById("color");
 let number = document.getElementById("number");
-
+let active = false;
 
 function createGrid() {
-    for (let i = 0; i < 50; i++){
+    colorValue = document.getElementById("color").value;
+    let value = document.getElementById("input").value;
+    number.textContent = `${value} x ${value}`;
+    clearGrid();
+    for (let i = 0; i < value * value; i++){
         const div = document.createElement("div");
         grid.appendChild(div);
         div.classList.add("gridsize");
-        grid.setAttribute("style", `grid-template-columns: repeat(8, 2fr); grid-template-rows: repeat(8, 2fr);`);
+        grid.setAttribute('style', `grid-template-columns: repeat(${value}, 2fr); grid-template-rows: repeat(${value}, 2fr);`);
     }
+    grid.style.boxShadow =`0 0 30px ${colorValue}`;
+    grid.style.borderColor = colorValue;
+    draw(colorValue) 
 }
 
 function clearGrid(){
@@ -20,25 +29,39 @@ function clearGrid(){
     }
 }
 
-input.addEventListener("input", updateValue)
-
-function updateValue() {
-    let value = document.getElementById("input").value
-    number.textContent = `${value} x ${value}`;
-    clearGrid();
-    for (let i = 0; i < value * value; i++) {
-        const div = document.createElement("div");
-        grid.appendChild(div);
-        div.classList.add("gridsize");
-    }
-    grid.setAttribute('style', `grid-template-columns: repeat(${value}, 2fr); grid-template-rows: repeat(${value}, 2fr);`);
+function draw(colorValue){                                           
+    let gridColors = document.querySelectorAll('.gridsize');
+    for (let gridColor of gridColors){
+        
+        gridColor.addEventListener('mousedown', () => {
+            gridColor.style.backgroundColor = colorValue;             
+        })  
+        
+        gridColor.addEventListener('mousemove', () => {
+            if (active)
+            gridColor.style.backgroundColor = colorValue;                 
+        })
+    }            
 }
 
-reset.addEventListener("click", resetPage)
+color.addEventListener('change', () => {   
+    colorValue = document.getElementById("color").value;     
+    draw(colorValue);
+    grid.style.boxShadow =`0 0 30px ${colorValue}`;
+    grid.style.borderColor = colorValue;  
+} )
 
-function resetPage() {
-    location.reload()
-}
+grid.addEventListener('mousedown', () => {      
+    active = true;  
+});   
 
-createGrid();
+grid.addEventListener('mouseup', () => {      
+    active = false;  
+});
+
+input.addEventListener("input", createGrid); 
+
+reset.addEventListener("click", () => { location.reload() });
+
+createGrid()
 
